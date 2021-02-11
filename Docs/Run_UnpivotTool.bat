30453dbe2af69e9c079901ea539622f3674dd4bb
alchemistmatt <matthew.monroe@pnnl.gov>
2021-02-11 14:40:52 -0800

    Update batch file to look for the executable

--- Docs/Run_UnpivotTool.bat
+++ Docs/Run_UnpivotTool.bat
@@ -1,4 +1,11 @@
 @echo off
+
+Set ProgramPath=UnpivotTool.exe
+
+If Exist ..\UnpivotTool.exe     Set ProgramPath=..\UnpivotTool.exe
+If Exist ..\Bin\UnpivotTool.exe Set ProgramPath=..\Bin\UnpivotTool.exe
+
 Rem The following will process file ExamplePivotTable.txt, creating file ExamplePivotTable_Unpivot.txt
 
-..\Bin\UnpivotTool.exe /I:ExamplePivotTable.txt /F:2 /B /N
+@echo on
+%ProgramPath% /I:ExamplePivotTable.txt /F:2 /B /N
